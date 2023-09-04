9b584b7a8209baba88d940f5d56438857f0bcfc2
Alexander <alexandercryptock@icloud.com>
2023-09-04 15:46:28 -0400

    added js question prompts

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  var length = prompt("Please enter desired password length")
+console.log(length)
+  var lowercase = confirm("Continue with lowercase letters?")
+  console.log(lowercase)
+  var uppercase = confirm("Continue with uppercase letters?")
+  console.log(uppercase)
+  var special = confirm("Contine with special characters?")
+  console.log(special)
+  var numbers = confirm("Continue with numbers?")
+  console.log(numbers)
+
+ if (lowercase || uppercase || special || numbers) {
+  // if yes to any than generate password
+  
+ } else{
+  // if none selected Alert none was selected
+ }
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
